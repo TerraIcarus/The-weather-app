6f9ab1adeaf5db23c786e3a20f49624aa9c9b650
Neon Rice <112954242+NeonRiceNon@users.noreply.github.com>
2022-11-02 17:33:43 +0300

    Add files via upload

---
+++ FX/src/main/java/com/example/fx/HelloApplication.java
@@ -0,0 +1,28 @@
+package com.example.fx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("bul.fxml"));
+            Scene scene = new Scene(root);
+            String css = this.getClass().getResource("style.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            stage.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
