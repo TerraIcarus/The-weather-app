34370430b98c3175634915c2aaa2b71a806fe094
Liliya M <liliyahaill89@gmail.com>
2022-11-03 23:11:39 +0300

    v1.1L

--- WeatherApp/src/main/java/com/example/weatherapp/HelloApplication.java
+++ WeatherApp/src/main/java/com/example/weatherapp/HelloApplication.java
@@ -10,12 +10,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+        try{
+            Parent root = FXMLLoader.load(getClass().getResource("bul.fxml"));
+            Scene scene = new Scene(root);
+            String css = this.getClass().getResource("style.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            stage.show();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     public static void main(String[] args) {
         launch();
